37c9a1c9e39eaeb413e1afc7945ac342a3f16e17
David Martin <david.martin@redhat.com>
2017-08-03 15:52:25 +0100

    Address PR feedback

--- ui/README.md
+++ ui/README.md
@@ -1,6 +1,11 @@
 # Mobile Control Panel UI
 
 
+## Prerequisites
+
+* `oc` cli >= 3.6.0-rc0 https://github.com/openshift/origin/releases (Known issue with service-catalog in 3.6.0)
+* Node.js >= 4(for install script(s))
+
 ## Local Installation
 
 Start openshift, configure it for MCP Extension development, and install the mobile apiserver
@@ -11,3 +16,9 @@ Start openshift, configure it for MCP Extension development, and install the mob
 
 The MCP Extension should now be visible in the OpenShift Web Console after the 'origin' container has restarted.
 Visit https://127.0.0.1:8443 to see the Console.
+
+To create a mobile app, use `oc` e.g.
+
+```
+oc create -f ../server/hack/install-apiserver/MobileApp.json
+```
--- ui/install.sh
+++ ui/install.sh
@@ -3,30 +3,30 @@
 set -x
 set -e
 
-# Mobile API Server repo location
-MOBILE_API_SERVER_REPO=github.com/feedhenry/mobile-apiserver
-MOBILE_API_SERVER_DIR=$GOPATH/src/$MOBILE_API_SERVER_REPO
+SCRIPT_PATH=$(dirname $0)
+SCRIPT_ABSOLUTE_PATH=$(cd $SCRIPT_PATH && pwd)
+
+# Mobile API Server install script location
+MOBILE_API_SERVER_DIR=$SCRIPT_ABSOLUTE_PATH/../server
 MOBILE_API_SERVER_INSTALL_SCRIPT=$MOBILE_API_SERVER_DIR/hack/install-apiserver/openshift/install.sh
 
-# Pull down the mobile-api server repo if not already cloned
-[[ -d $MOBILE_API_SERVER_DIR ]] || go get -u $MOBILE_API_SERVER_REPO
+# master-config.yaml location
+OPENSHIFT_CONFIG_DIR=$SCRIPT_ABSOLUTE_PATH
+OPENSHIFT_MASTER_CONFIG=$OPENSHIFT_CONFIG_DIR/master/master-config.yaml
+MCP_BASE_DIR=$OPENSHIFT_CONFIG_DIR
 
 # Start OpenShift with the current directory as the config dir
 # Using the current directory as the config dir is important for extension development
 # This allows changing of extension files and having them already mounted in the origin container
-oc cluster up --service-catalog --host-config-dir=${PWD} --version='v3.6.0-rc.0'
+oc cluster up --service-catalog --host-config-dir=${OPENSHIFT_CONFIG_DIR} --use-existing-config=true --version='v3.6.0-rc.0'
 
 # Grant unauthenticated access to the template service broker api
 oc login -u system:admin
 oc adm policy add-cluster-role-to-group system:openshift:templateservicebroker-client system:unauthenticated system:authenticated
 
-# master-config.yaml location
-OPENSHIFT_CONFIG_DIR=$PWD
-OPENSHIFT_MASTER_CONFIG=$OPENSHIFT_CONFIG_DIR/master/master-config.yaml
-MCP_BASE_DIR=$OPENSHIFT_CONFIG_DIR
-
 # Enable Extension Development
 sudo chmod 666 $OPENSHIFT_MASTER_CONFIG
+cd $SCRIPT_ABSOLUTE_PATH
 npm i
 node update_master_config.js $OPENSHIFT_MASTER_CONFIG
 sudo chmod 644 $OPENSHIFT_MASTER_CONFIG
